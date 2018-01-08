174b3d7a46ce7d675d5f323c436a2bfc05c30f2b
rcol4jc@hotmail.com <Rick Collins>
2018-01-08 00:33:38 -0600

    php and updated apache file

--- Provisionfiles/provision-deb-apache.sh
+++ Provisionfiles/provision-deb-apache.sh
@@ -7,13 +7,6 @@ echo " "
 
 sudo apt-get install apache2 apache2-utils -y
 
-echo "*************************"
-echo "* Create link between   *"
-echo "* /var/www/html and     *"
-echo "* /vagrant/html         *"
-echo "*************************"
-echo " "
-sudo ln -s /var/www/html /vagrant/html
 
 
 
---
+++ Provisionfiles/provision-deb-php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "***************************"
+echo "* install php             *"
+echo "***************************"
+
+sudo apt-get install php5 php5-mysql php5-gd -y
